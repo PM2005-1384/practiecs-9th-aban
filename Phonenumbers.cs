766a7c2ab826b805251c97167de516c5650cc019
PM2005-1384 <memarfarp@gmail.com>
2024-11-03 18:42:02 +0330

    Add files via upload

---
+++ Phonenumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+    Console.WriteLine("please enter your text, the program will extract the phone numbers in a moment");
+    Console.ReadLine();
+
+var phone_numbers = new List<string>();
+
+foreach (var number in phone_numbers)
+{
+    if (number.Length == 10 && number.StartsWith('0') && number.Distinct().Count() == 10)
+    {
+        phone_numbers.Add(number);
+    }
+
+}
+    Console.WriteLine("here are the phone numbers from the written text:");
+foreach (var phone_number in phone_numbers)
+{
+    Console.WriteLine(phone_number);
+}
+
+
